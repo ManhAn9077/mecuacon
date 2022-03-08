2e1212c22d01ed5446b40d0a12f9c3eb87642174
ManhAn9077 <94302143+ManhAn9077@users.noreply.github.com>
2022-03-08 16:10:17 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'MẸ CỦA CON';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng 08/03';
+const titleCard = 'MẸ CỦA CON';
+const contentCard = 'Chúc mẹ của con có một ngày 8/3 vui vẻ , con gái mong mẹ sẽ luôn nở nụ cười trên môi ; gặp may mắn ,thành công trong công việc';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'https://scontent.fhan4-3.fna.fbcdn.net/v/t1.6435-9/149363447_947771262424722_6458477021303319324_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=174925&_nc_ohc=asjlirzuBoQAX_18ZWf&_nc_ht=scontent.fhan4-3.fna&oh=00_AT80Iw3vXk05LOJ9GdN5_lFOAHThGPPuqUWtnJRkGXxzgA&oe=624B501F';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
